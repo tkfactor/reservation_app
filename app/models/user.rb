c8a287db4f225b727eb1bb7c08b8208a9189abe8
t k <tk@tinoMacBook-Pro.local>
2022-08-17 12:10:51 +0900

    自己紹介と画像登録のバリデーションを外しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,4 @@ class User < ApplicationRecord
   has_many:reservations
 
   validates :name, presence: true
-  validates :user_introduction, presence: true, length: {maximum: 100}
-  validates :image, presence: true
 end
